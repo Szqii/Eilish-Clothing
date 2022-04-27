cf42672efd4519a2fb5932d95d7e2333c257f71a
Sezgi Altan <szqii.altn@gmail.com>
2022-04-27 10:39:05 +0300

    Add :: Category Item Component

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,19 @@
+import "./category-item.styles.scss";
+
+const CategoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>SHOP NOW</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
