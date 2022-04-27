53509d93fa47dd8e04289bb6da65b4ffa7ed0066
Sezgi Altan <szqii.altn@gmail.com>
2022-04-27 10:40:49 +0300

    Add :: Homepage Component

---
+++ src/routes/homepage/homepage.component.jsx
@@ -0,0 +1,35 @@
+import Directory from "../../components/directory/directory.component";
+
+const Homepage = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "HATS",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "JACKETS",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "SNEAKERS",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "WOMENS",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "MENS",
+      imageUrl: "https://i.ibb.co/R70vBrQ/mens.png",
+    },
+  ];
+
+  return <Directory categories={categories} />;
+};
+
+export default Homepage;
