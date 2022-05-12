15558fdb59b760e8fce397e0ebd240bf0bce05db
Sezgi Altan <szqii.altn@gmail.com>
2022-05-12 22:47:35 +0300

    Update :: Dynamic Sign in/out text

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,13 @@
-import { Fragment } from "react";
+import { async } from "@firebase/util";
+import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as EilishLogo } from "../../assets/eilish-logo.svg";
-
+import { UserContext } from "../../contexts/user.context";
+import { SignOutUser } from "../../utils/firebase/firebase.utils";
 import "./navigation.styles.scss";
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
   return (
     <Fragment>
       <div className="navigation">
@@ -15,9 +18,15 @@ const Navigation = () => {
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className="nav-link" to="/auth">
-            SIGN IN
-          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={SignOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              SIGN IN
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
