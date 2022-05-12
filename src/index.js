262950e4406a6f33106189692203ab399612a60e
Sezgi Altan <szqii.altn@gmail.com>
2022-05-12 22:42:40 +0300

    Add :: User Provider

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.scss";
 import App from "./App";
+import { UserProvider } from "./contexts/user.context";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
