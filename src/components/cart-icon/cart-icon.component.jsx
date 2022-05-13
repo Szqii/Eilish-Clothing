7f8975feacf95602e29f485c1d61965214c8f339
Sezgi Altan <szqii.altn@gmail.com>
2022-05-13 18:30:11 +0300

    Create :: Cart Icon Component

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,17 @@
+import "./cart-icon.styles.scss";
+import { useContext } from "react";
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import { CartContext } from "../../contexts/cart.context";
+
+const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const toggleCart = () => setIsCartOpen(!isCartOpen);
+  return (
+    <div className="cart-icon-container" onClick={toggleCart}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">0</span>
+    </div>
+  );
+};
+
+export default CartIcon;
