f6fd18dbc6cdfba0b531f0862bb64de790ada101
Sezgi Altan <szqii.altn@gmail.com>
2022-05-13 18:32:14 +0300

    Update :: Shop Component Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Homepage from "./routes/homepage/homepage.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Auth from "./routes/auth/auth.component";
+import Shop from "./routes/shop/shop.component";
 import { Route, Routes } from "react-router-dom";
 
 const App = () => {
@@ -8,7 +9,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Homepage />} />
-        <Route path="shop"> Shop Page</Route>
+        <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Auth />} />
       </Route>
     </Routes>
