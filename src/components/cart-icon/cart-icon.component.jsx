54d50556fa51f452a7af7f5790378f5d7b9e30b8
Sezgi Altan <szqii.altn@gmail.com>
2022-05-14 19:39:06 +0300

    Update :: Visual improvements on cart icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,12 +4,13 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { CartContext } from "../../contexts/cart.context";
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, totalQuantity } = useContext(CartContext);
   const toggleCart = () => setIsCartOpen(!isCartOpen);
+
   return (
     <div className="cart-icon-container" onClick={toggleCart}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{totalQuantity}</span>
     </div>
   );
 };
