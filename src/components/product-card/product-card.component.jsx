daf7b3cb89f2b1bddd5ef6245fd954e09437a37d
Sezgi Altan <szqii.altn@gmail.com>
2022-05-17 16:58:14 +0300

    Fix :: Typo

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -13,7 +13,7 @@ const ProductCard = ({ product }) => {
       <img src={imageUrl} alt={`${name}`} />
       <div className="footer">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        <span className="price">${price}</span>
       </div>
       <Button buttonType="inverted" onClick={addProductToCart}>
         Add to cart
