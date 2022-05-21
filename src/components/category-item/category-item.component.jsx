c146989033a0abe97ae6dcdaa990240c4e3b15d2
Sezgi Altan <sezgialtan@Sezgis-Mac.local>
2022-05-22 00:43:00 +0300

    Update :: Container Class and Style

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -3,7 +3,7 @@ import "./category-item.styles.scss";
 const CategoryItem = ({ category }) => {
   const { imageUrl, title } = category;
   return (
-    <div className="category-container">
+    <div className="category-item-container">
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
