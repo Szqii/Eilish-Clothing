bd4df02b37e65b4c435eaddc30d9fe3ef2db11ce
Sezgi Altan <sezgialtan@Sezgis-Mac.local>
2022-05-22 00:45:02 +0300

    Update :: Shop Component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,16 +1,15 @@
-import { useContext } from "react";
-import { ProductsContext } from "../../contexts/product.context";
-import ProductCard from "../../components/product-card/product-card.component";
 import "./shop.styles.scss";
 
+import { Route, Routes } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
+
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
   return (
-    <div className="products-container">
-      {products.map((product) => {
-        return <ProductCard key={product.id} product={product} />;
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 
