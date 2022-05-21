96d1b53171cc310ac73628233173b9f549c9e953
Sezgi Altan <sezgialtan@Sezgis-Mac.local>
2022-05-22 00:37:58 +0300

    Update :: Route

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Homepage />} />
-        <Route path="shop" element={<Shop />} />
+        <Route path="shop/*" element={<Shop />} />
         <Route path="auth" element={<Auth />} />
         <Route path="checkout" element={<Checkout />} />
       </Route>
