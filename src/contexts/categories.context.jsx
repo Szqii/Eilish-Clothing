290af2a04f5c495171d7373a1062a417b0f1eaaa
Sezgi Altan <sezgialtan@Sezgis-Mac.local>
2022-05-22 00:45:40 +0300

    Create :: Categories Context

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,29 @@
+import { createContext, useState, useEffect } from "react";
+import {
+  adddCollectionAndDoc,
+  getCategoriesAndDocuments,
+} from "../utils/firebase/firebase.utils.js";
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const categoryMap = await getCategoriesAndDocuments("categories");
+      setCategoriesMap(categoryMap);
+    };
+    getCategories();
+  }, []);
+
+  const value = { categoriesMap };
+
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
