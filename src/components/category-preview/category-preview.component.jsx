984e5f5a9878e0ea27f48a64c9d6fb873ea24077
Sezgi Altan <sezgialtan@Sezgis-Mac.local>
2022-05-22 00:48:01 +0300

    Create :: Category Preview Component

---
+++ src/components/category-preview/category-preview.component.jsx
@@ -0,0 +1,21 @@
+import "./category-preview.styles.scss";
+import ProductCard from "../product-card/product-card.component.jsx";
+
+const CategoryPreview = ({ title, products }) => {
+  return (
+    <div className="category-preview-container">
+      <h2>
+        <span className="title">{title.toUpperCase()}</span>
+      </h2>
+      <div className="preview">
+        {products
+          .filter((_, index) => index < 5)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPreview;
