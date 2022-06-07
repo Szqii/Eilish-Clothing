32c55ed1e6d24026fcbaef8e18cf4637086d84f3
Sezgi Altan <sezgialtan@Sezgis-Mac.local>
2022-06-07 23:46:36 +0300

    Update :: Category preview component

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,11 +1,15 @@
 import "./category-preview.styles.scss";
 import ProductCard from "../product-card/product-card.component.jsx";
+import { Link } from "react-router-dom";
+import { ReactComponent as Arrow } from "../../assets/right-arrow.svg";
 
 const CategoryPreview = ({ title, products }) => {
   return (
     <div className="category-preview-container">
       <h2>
-        <span className="title">{title.toUpperCase()}</span>
+        <Link className="title" to={title}>
+          {title.toUpperCase()} <Arrow className="arrow" />
+        </Link>
       </h2>
       <div className="preview">
         {products
