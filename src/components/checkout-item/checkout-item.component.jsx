0d8f865bfd75de77fa47a32f0edfeed9c488bdcf
Sezgi Altan <sezgialtan@Sezgis-Mac.local>
2022-06-07 23:47:07 +0300

    Fix :: Typo

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -26,7 +26,7 @@ const CheckoutItem = ({ cartItem }) => {
           &#10095;
         </div>
       </span>
-      <span className="price">{price}</span>
+      <span className="price">${price}</span>
       <div className="remove-button" onClick={clearItem}>
         &#10005;
       </div>
